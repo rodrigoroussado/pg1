5f0520a57378d244c951f2e6e732811805dc80e3
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-09-30 16:41:42 +0100

    Update oblong_numbers.c (ainda com bug a determinar oblongo ou não)

--- trab1/oblong_numbers.c
+++ trab1/oblong_numbers.c
@@ -4,8 +4,20 @@
 //Função que retorna true se um número for oblongo
 bool is_oblong(int n){
 	
-	
-	
+	for(int i=0; i != n; i++){
+		
+				
+		if( (n = i*(i+1)) ){
+			
+			break;
+			
+			}
+
+			return false;
+
+		}
+
+	return true;
 	}
 
 
@@ -15,7 +27,10 @@ int main(){
 	int n;
 
 //Apresentar o programa
-//While loop
+	printf("Programa que indica se um número é oblongo.\n");
+	printf("Digite número e termine com um caractere não dígito:\n");
+
+//While loop (pede um número ao utilizador até este terminar com um caractere não dígito
 	while(scanf("%d",&n) == 1){
 		
 		if(is_oblong(n) == true){
