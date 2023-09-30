ba03e7336b1471035bc015dad725c1b0669b1ad9
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-09-30 16:19:53 +0100

    Create oblong_numbers.c (ainda sem a função)

---
+++ trab1/oblong_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+//Função que retorna true se um número for oblongo
+bool is_oblong(int n){
+	
+	
+	
+	}
+
+
+int main(){
+	
+//Declarar variáveis
+	int n;
+
+//Apresentar o programa
+//While loop
+	while(scanf("%d",&n) == 1){
+		
+		if(is_oblong(n) == true){
+			
+			printf("É oblongo\n");
+			
+			}
+		
+		}
+
+}
