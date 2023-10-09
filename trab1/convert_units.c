dd0e00fc9faa6eabab9b6082a650107246d806c6
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-10-09 17:40:05 +0100

    convert_units.c metade feito

--- trab1/convert_units.c
+++ trab1/convert_units.c
@@ -1,52 +1,76 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
 
-//Função que encontra a solução de i²+i-n = 0
-double zerosSegundoGrau(int a, int b, int c, double *z1, double *z2){
-	// a = 1; b = 1; c = -n
+bool validarValor(double valor){
 	
+	if(valor < 0){return false;}
+	else{return true;}
 	
-	
-	double aux = (b*b)-4*a*c;
-	if(aux < 0){return false;}
-		
-	*z1 = (-b + sqrt(aux))/2*a;
-	*z2 = (-b - sqrt(aux))/2*a;
-	
-}
-
-	
+	}
 
 
-//Função que retorna true se um número for oblongo
-bool is_oblong(int n){
+bool validarUnidade(int unidade){
 	
-	int i = 0;
-	double z1, z2;
+	if(unidade < 1 || unidade > 7){return false;}
+	else{return true;}
 	
-	if(n < 0){return false;}
+	}
+	
+
+bool verificarInteiro(double resultado){
+	
+	int resultadoInt = resultado;
+	
+	if((resultado = resultadoInt)){return true;}
+	else{return false;}
+	
+	}
+
+int main(){
+	
+	//Declarar variáveis
+	double valor;
+	int uni_inicial, uni_final;
+	int fator_mult;
+	
+	//Apresentar o programa
+	printf("Lê um valor e converte-o para qualquer unidade de comprimento (mm,cm,dm,m,dam,hm,km)\n");
+	printf("Escreva um número de 1-7 para definir a unidade inicial e a unidade para a qual pretende converter!\n");
+	printf("1 = mm, 2 = cm, 3 = dm, 4 = m, 5 = dam, 6 = hm, 7 = km \n");
+	
+	//Receber valor
+	printf("Insira o valor a converter: ");
+	if( scanf("%lf",&valor)!= 1 ){printf("Valor inválido!\n");}
 	else{
 		
-		if(n%2 != 0){return false;}
+		if(validarValor(valor) == false){printf("Valor inválido!\n");}
 		else{
 			
-			if( i*i + i > n){return false;}
+			//Receber unidade inicial e validar
+			printf("Insira a unidade inicial: ");
+			
+			if( scanf("%d",&uni_inicial)!= 1 ){printf("Unidade inicial inválida!\n");}
 			else{
 				
-				zerosSegundoGrau(1,1,-n,&z1,&z2);
-
-				for(int i = 0; i <= n; i++){
-		
-					if( z1 == 0 || z2 == 0){  //Se a função retornar 0: é oblongo!
-			
-						return true;
-						break;
-			
+				if(validarUnidade(uni_inicial) == false){printf("Unidade inicial inválida!\n");}
+				else{
+					
+					//Receber unidade final e validar
+					printf("Insira a unidade final: ");
+					
+					if( scanf("%d",&uni_final)!= 1 ){printf("Unidade final inválida!\n");}
+					else{
+						
+						if(validarUnidade(uni_final) == false){printf("Unidade final inválida!\n");}
+						else{
+							
+							//Calcular a diferença entre a unidade final e a inicial
+							
+							
+							}
+						
 						}
-		
-					return false;
-		
+					
 					}
 				
 				}
@@ -54,29 +78,7 @@ bool is_oblong(int n){
 			}
 		
 		}
-
-}
-
-
-int main(){
 	
-//Declarar variáveis
-	int n;
-
-
-//Apresentar o programa
-	printf("Programa que indica se um número é oblongo.\n");
-	printf("Digite número e termine com um caractere não dígito:\n");
-
-//While loop (pede um número ao utilizador até este terminar com um caractere não dígito
-	while(scanf("%d",&n) == 1){
-		
-		if(is_oblong(n) == true){
-			
-			printf("É oblongo\n");
-			
-			}
-		
-		}
-
+	
+	
 }
