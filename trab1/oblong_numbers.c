f91837d10c12e6ead4fabe7808eff942f20f819e
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-10-10 08:49:15 +0100

    oblong_numbers.c feito e testado

--- trab1/oblong_numbers.c
+++ trab1/oblong_numbers.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
 //Função que retorna true se um número for oblongo
-bool is_oblong(int n){
-	
-	for(int i=0; i != n; i++){
+int is_oblong(int n){
+
+	int aux = 0; //Variável que, caso seja = 1, o número é oblongo, caso seja = 0, o número não é oblongo
+
+	if(n < 0){return aux;}
+	else{
 		
-				
-		if( (n = i*(i+1)) ){
+		if(n%2 != 0){return aux;}
+		else{
 			
-			break;
+			for(int i = 0; i <= n; i++){
+		
+				if(n == i*(i+1)){aux = 1; break;}
+				
+		
+				}	
+			
+			return aux;
 			
 			}
-
-			return false;
-
+		
 		}
 
-	return true;
-	}
+
+
+}
 
 
 int main(){
 	
 //Declarar variáveis
 	int n;
+
 
 //Apresentar o programa
 	printf("Programa que indica se um número é oblongo.\n");
