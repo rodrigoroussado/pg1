ee51ae8d00e9168f1f4f269d5c88a5dc5c774e14
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-10-18 11:57:43 +0100

    Create ordenarArray.c

---
+++ exemplos_aulas/ordenarArray.c
@@ -0,0 +1,25 @@
+//Ordenar um array
+
+void sort(int info[], int dim){
+
+	for(int i = 0; i < dim-1; i++){
+
+		int posMenor = i;
+		for(int j = i+1; j < dim; j++){				//Procura a posição do menor valor
+
+			if(info[j] < info[posMenor]){posMenor = j;}	//Encontrou um menor que o anterior menor
+
+		}
+
+		if(posMenor != i){	//Se o novo menor não for igual ao menor anterior, troca os de posição
+
+			int aux = info[i];
+			info[i] = info[posMenor];
+			info[posMenor] = aux;
+
+
+		}
+
+	}
+
+}
