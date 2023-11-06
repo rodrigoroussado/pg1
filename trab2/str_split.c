0726755a5da30197d8d0b8795c8eb4623c2a36f7
João Santos <145870334+johnnyfhd@users.noreply.github.com>
2023-11-06 13:03:20 +0000

    Update str_split.c

--- trab2/str_split.c
+++ trab2/str_split.c
@@ -1,34 +1,42 @@
 int str_split(char text[], word_t words[], int size) {
-    int word_count = 0;
-    char* start = text;
-    char* end = text;
+	
+    int contador = 0;
+    char* inicio = text;
+    char* fim = text;
 
-    while (*end != '\0' && word_count < size) {
+    while (*fim != '\0' && contador < size) {
 		//encontra o inicio da palavra tirando tudo o que ´eespaço e mudanaças de linha ou mesmo tab
-        while (*start != '\0' && (*start == ' ' || *start == '\t' || *start == '\n')) {
-            start++;
+        while (*inicio != '\0' && (*inicio == ' ' || *inicio == '\t' || *inicio == '\n')) {
+			
+            inicio++;
+            
         }
 
-        if (*start == '\0') {   //caso a string esteja vazia e chegamos ao fim saimos so while
+        if (*inicio == '\0') {   //caso a string esteja vazia e chegamos ao fim saimos so while
+			
             break;
+            
         }
-		// end = start para começar a ler de onde parou anteriormente para agora encontrat o fim da palavra
-        end = start;
+		// fim = inicio para começar a ler de onde parou anteriormente para agora encontrat o fim da palavra
+        fim = inicio;
 
-        while (*end != '\0' && (*end != ' ' && *end != '\t' && *end != '\n')) {
-            end++;
+        while (*fim != '\0' && (*fim != ' ' && *fim != '\t' && *fim != '\n')) {
+			
+            fim++;
+            
         }
 
-        int word_length = end - start;
+        int word_length = fim - inicio;
 
 
-        strncpy (words[word_count], start, word_length);    //copia "start" vezes para words 
-        words[word_count][word_length] = '\0';             //adiciona \0 no final da string!!
+        strncpy (words[contador], inicio, word_length);    //copia "start" vezes para words 
+        words[contador][word_length] = '\0';             //adiciona \0 no final da string!!
 
-        word_count++;                                     //vai sempre interando ate end = \0
-        start = end;
+        contador++;                                     //vai sempre interando ate end = \0
+        inicio = fim;
     }
 
-    return word_count;
+    return contador;
 
+}
 }
