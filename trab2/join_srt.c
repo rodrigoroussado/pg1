f1f310bfde0773ed060b7ac837d34b4727135a43
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-11-13 18:31:56 +0000

    join_srt.c a juntar os ficheiros (sem update de hora e nº)

--- trab2/join_srt.c
+++ trab2/join_srt.c
@@ -1,131 +1,61 @@
-/*Programa que junta dois ficheiros .srt (Subtitle Ripple) em apenas um*/
-
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define MAX_FILENAME 100
-#define MAX_TIME 12
-#define MAX_LINE 1000
+int main() {
+    FILE *file1, *file2, *output;
+    char line[1000];
 
-//Definir tipos que suportam o nome do ficheiro e o tempo da legenda
-typedef char FileName[MAX_FILENAME + 1];
-typedef char TimeValue[MAX_TIME];
+    // Abre os dois arquivos .srt para leitura
+    file1 = fopen("padrinho2_1.srt", "r");
+    file2 = fopen("padrinho2_2.srt", "r");
 
+    if (file1 == NULL || file2 == NULL) {
+        perror("Erro ao abrir um dos arquivos.");
+        exit(EXIT_FAILURE);
+    }
 
-bool addTime(h1,m1,s1,ms1,h2,m2,s2,ms2,*hr,*mr,*sr,*msr){
-	
-	*hr = h1 + h2;
-	if(*hr > 24){return false;}
-	
-	*mr = m1 + m2;
-	if(*mr >= 60){*hr++; *mr-= 60;}
-	
-	*sr = s1 + s2;
-	if(*sr >= 60){*mr++; *sr-=60}
-	
-	*msr = ms1 + ms2;
-	if(*msr > 999){*sr++; *,sr-=999;}
-	
-	return true;
-	
-}
+    // Abre o arquivo de saída para escrita
+    output = fopen("output.srt", "w");
+    if (output == NULL) {
+        perror("Erro ao abrir o arquivo de saída.");
+        exit(EXIT_FAILURE);
+    }
 
-//Função que recebe o nome do ficheiro e o abre
-FILE * readFileName(char msg[],FileName filename, char modo[]){
-	
-	int tamanhoFile = strlen(filename);
-	FILE *f = NULL;
-	
-	while(true){
-		
-		printf("%s",msg);
-		
-		fgets(filename,tamanhoFile,stdin);
-		int len = strlen(filename);
-		
-		if(filename[len-1] == '\n'){filename[len-1] = '\0';} //Se existir, retira o return no fim do ficheiro
-		
-		f = fopen(filename,modo);
-		
-		if(f != NULL){break;}
-		
-	}
-	
-	return f;
-	
-}
+    // Processa o primeiro arquivo .srt
+    while (fgets(line, sizeof(line), file1) != NULL) {
+        fprintf(output, "%s", line);
 
-//Função que mostra erro quando não é possível ler o ficheiro
-void erroConversao(){
-	
-	printf("Erro ao juntar os ficheiros!");
-	
-}
+        // Verifica se a linha contém o número da legenda e o tempo
+        if (sscanf(line, "%*d:%*d:%*d,%*d --> %*d:%*d:%*d,%*d") == 0) {
+            // Incrementa o número da legenda
+            int number;
+            sscanf(line, "%d", &number);
+            
+        }
+    }
 
+    // Adiciona uma linha em branco para separar os conteúdos
+    fprintf(output, "\n");
 
-//Função que processa as legendas
-bool processarLegendas(FILE *fin, FILE *fout ,int *nlegenda, TimeValue timeStart){
+    // Processa o segundo arquivo .srt
+    while (fgets(line, sizeof(line), file2) != NULL) {
+        fprintf(output, "%s", line);
 
-	int hrs, min, seg, mseg;
-	int hrsS, minS, segS, msegS;
-	int hrsE, minE, segE, msegE;
-	
-	char middle[MAX_LINE];
-	char line[MAX_LINE];
-	
-	TimeValue timeS, timeE;
+        // Verifica se a linha contém o número da legenda e o tempo
+        if (sscanf(line, "%*d:%*d:%*d,%*d --> %*d:%*d:%*d,%*d") == 0) {
+            // Incrementa o número da legenda
+            int number;
+            sscanf(line, "%d", &number);
+            
+        }
+    }
 
-	
-	while(fgets(line,MAX_LINE,fin) != NULL){
-		
-		fprintf(fout,"%d\n", ++(*nlegenda));
-		
-		if(fgets(line, MAX_LINE, fin) == NULL){erroConversao();}
-			
-		scanf(line,"%s:%s:%s", timeS,middle,timeE);
-		
-		addTime(hrs,min,seg,mseg,hrsS,minS,segS,msegS,timeS);
-		addTime(hrsS,minS,segS,msegS,hrsE,minE,segE,msegE,timeE);
-		
-		fprintf(fout,"%s:%s:%s,%s",timeS,middle,timeE);
-		
-	}
-	
-	return true;
-	
-}
+    // Fecha os arquivos
+    fclose(file1);
+    fclose(file2);
+    fclose(output);
 
+    printf("Arquivos .srt combinados com sucesso!\n");
 
-
-
-int main(){
-	
-	FileName filenamePart1, filenamePart2, filenameResult;
-	TimeValue endTime = "00:00:00,000";
-	
-	int legendNumber;
-	
-	
-	FILE *f1 = readFileName("Parte 1: ",filenamePart1,"r");
-	if(f1 == NULL){erroConversao();}
-	
-	FILE *f2 = readFileName("Parte 2: ",filenamePart2,"r");
-	if(f2 == NULL){erroConversao();}
-	
-	FILE *fout = readFileName("Resultado: ", filenameResult, "w");
-	if(fout == NULL){erroConversao();}
-	
-	//Processar as legendas, atualizando o seu número e tempo e validar o formato de tempo
-	bool res1 = processarLegendas(f1,fout,&legendNumber,endTime);
-	bool res2 = processarLegendas(f2,fout,&legendNumber,endTime);
-
-	
-	//Fechar os ficheiros que foram abertos
-	fclose(fout);
-	fclose(f1);
-	fclose(f2);
-	
-	return true;
-	
+    return 0;
 }
