ddad5695710fde4417f16689cfe76d8dd395aaf4
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-11-14 16:51:06 +0000

    BIGINT PRONTO PARA ENTREGA

--- trab2/Trabalho_2_G02_A51009_A50977/bigint.c
+++ trab2/Trabalho_2_G02_A51009_A50977/bigint.c
@@ -1,3 +1,6 @@
+//Grupo 02
+//Alunos João Santos (nº51009) e Rodrigo Viegas (nº50977)
+//LT13D
 
 #include "bigint.h"
 
@@ -64,20 +67,26 @@ bool big_from_string(char n[], BIG_INT big) {
 	int posicao_terminador = 0;
 	int j = 0;		//Variável usada para colocar os valores em big
 	
+	//Percorrer a string até encontrar o terminador, ficando com o índice
+	//do terminador na string na variável "posicao_terminador"
 	for(int i = 0; n[i] != '\0'; i++){posicao_terminador++;}
 	
+	//Se a string for vazia, não é possível fazer a conversão para big int
 	if(n[0] == '\0'){return false;}
 	
 	for(int i = posicao_terminador - 1; i >= 0; i--){
 		
-		if(n[i] >= '0' && n[i] <= '9'){
+		//Validar se o que temos na string é um caractere numérico
+		if(n[i] >= '0' && n[i] <= '9'){	
 		
-			big[j+1] = n[i] - '0'; 		//i + 1 porque deixamos um espaço (big[0]) para colocar o tamanho do big int
+			//j + 1 porque deixamos um espaço (big[0]) para colocar o tamanho do big int
+			big[j+1] = n[i] - '0';
 			
 			tamanho++;
 			
 		}
 		
+		//Se encontrarmos algum caractere não numérico na string, a função retorna falso
 		else{return false;}
 		
 		j++;
@@ -107,10 +116,15 @@ void big_from_long(long n, BIG_INT big) {
 	//Dividir o long por 10 e obter o seu dígito de menor peso (resto da divisão)
 	while(n >= 1){
 		
+		//Obter o dígito de menor peso e colocá-lo na próxima posição do array
 		digito = n%10;
 		big[i] = digito;
 		
+		//Atualizar o valor de n para o próximo ciclo do "while"
 		n = n/10;
+		
+		//Incrementar o contador que, no fim do "while" nos indicará qual o número
+		//de dígitos do big int
 		contador++;
 		
 		}
@@ -134,6 +148,9 @@ void big_from_long(long n, BIG_INT big) {
  *    valor positivo -> b1 > b2
  */ 
  
+//Função que calcula e retorna o número de dígitos de um big int, ignorando os 
+//zeros à esquerda do número (que, neste caso, estão nas posições mais à
+//direita do array)
 static int digitosReais (const BIG_INT b){
 	
 	int zeros = 0;
@@ -151,17 +168,20 @@ int big_cmp(const BIG_INT b1, const BIG_INT b2) {
     
     int diferenca;
     
-    //b1 > b2
+    //Se b1 tiver mais "dígitos reais" (ignorando os zeros à esquerda) que b2
+    //sabemos que b1 > b2 (retornamos um valor positivo)
 	if( digitosReais(b1) > digitosReais(b2) ){return 1;}
 		
 	else{
 		
-		//b1 < b2
+		//Se b1 tiver menos "dígitos reais" que b2, sabemos que b1 < b2
+		//(retornamos um valor negativo)
 		if( digitosReais(b1) < digitosReais(b2) ){return -1;}
 		
 		else{
 			
-			//b1 == b2
+			//Se b1 e b2 tiverem o mesmo número de "dígitos reais", temos
+			//de avaliar dígito a dígito até encontrarmos uma diferença
 			for(int i = digitosReais(b1); i != 0; i--){
 			
 				diferenca = b1[i] - b2[i];
@@ -170,7 +190,9 @@ int big_cmp(const BIG_INT b1, const BIG_INT b2) {
 			}
 		}
 	}
-	
+			
+	//Se compararmos b1 e b2 dígito a dígito e não houver nenhuma diferença
+	//retornamos 0, indicando que os big ints são iguais
 	return 0;
 	
 }
@@ -219,7 +241,9 @@ bool big_add(const BIG_INT b1, const BIG_INT b2, BIG_INT bm) {
 		bm[i] = c % 10;
 	}
     
-	//Aumentar sempre 1 no tamanho da array quando o carry for 1
+	//Se, no fim da soma, ainda houver overflow, coloca o valor de overflow 
+	//na respetiva posição do array e incrementa o tamanho do big int
+	
 	if (carry == 1) {
 		
 		bm[tamanho_bm + 1] = carry;
@@ -227,9 +251,10 @@ bool big_add(const BIG_INT b1, const BIG_INT b2, BIG_INT bm) {
 		tamanho_bm++;
 	}
     
-	//Retorna falso caso tenha havido OVERFLOW
+	//Retorna falso caso tenha havido overflow
 	if( bm[tamanho_bm + 1] >= MAX_DIGITS ){return false;}
 	
+	//Atualiza o tamanho do big int para o novo valor (pós soma)
 	bm[0] = tamanho_bm;
 	return true;	
 }
@@ -265,11 +290,10 @@ bool big_mul_dig(const BIG_INT b, int d, BIG_INT bres) {
 	} 
 	else{
 		
-		//Somar d vezes 
+		//Somar d vezes (multiplicação por somas sucessivas)
 		while( d > 1){
 			
 			if(big_add(bres, b, bres) == false){return false;}
-			
 			
 			d--;
 			
@@ -292,6 +316,7 @@ bool big_mul_dig(const BIG_INT b, int d, BIG_INT bres) {
  */
 bool big_mul_10(BIG_INT b) {
 	
+	//Se a multiplicação causar overflow, a função retorna falso
 	if(big_size(b) + 1 > MAX_DIGITS){return false;}
     
     //Percorrer a array b, colocando todos os elementos uma posição para a frente
@@ -332,18 +357,17 @@ bool  big_mul_pow_10(BIG_INT b, int exp) {
 	
 	int digitos = big_size(b);
     
+    //Caso a multiplicação cause overflow, a função retorna falso
     if(digitos + exp > MAX_DIGITS){return false;}
     
+    //Multiplicar o número por 10, exp vezes
     for(int i = 0; i < exp; i++){
 		
-		big_mul_10(b);
+		big_mul_10(b);				
 		
 	}
 	
 	return true;
-    
-    
-    
 }
 
 
@@ -360,10 +384,5 @@ bool  big_mul_pow_10(BIG_INT b, int exp) {
  */	
 bool big_mul(const BIG_INT b1, const BIG_INT b2, BIG_INT bm) {
     // TO IMPLEMENT
-    
-    //MULTIPLICAÇÃO DE MATRIZES!!!
-    
 	return false;
 }
- 
- 
