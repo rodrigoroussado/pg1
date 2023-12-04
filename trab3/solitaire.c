4ab26c6c1927f5379a6c8fd6196e18f3a8b33765
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-12-04 12:24:37 +0000

    Update solitaire.c

--- trab3/solitaire.c
+++ trab3/solitaire.c
@@ -1,1 +1,23 @@
+//Fazer includes
+#include <stdio.h>
+#include <stdbool.h>
+#include "graphics.h"
+#include "cards.h"
 
+
+//Definir estruturas
+
+
+//Definir variáveis globais
+
+	
+
+//Main
+int main(){
+	
+	graph_init2("Solitaire", MAX_WIDTH, MAX_HEIGHT);
+	//game_init();
+	background_draw();
+	graph_start(); //DEFINIR EVENTOS DE RATO AQUI!
+	
+}
