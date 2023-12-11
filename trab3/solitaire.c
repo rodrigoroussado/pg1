51b7a6bc6c64ce747cd8f8de6bbec3dcd12b63c0
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-12-11 11:59:35 +0000

    Update solitaire.c

--- trab3/solitaire.c
+++ trab3/solitaire.c
@@ -4,10 +4,11 @@
 #include "graphics.h"
 #include "cards.h"
 
-Board board = {[],0,0,0};
+
+
 //void mouse(){}
 //void timer(){}
-
+Board board;
 
 
 	
@@ -16,10 +17,11 @@ Board board = {[],0,0,0};
 int main(){
 	
 	graph_init2("Solitaire", MAX_WIDTH, MAX_HEIGHT);
-	//mouse_register(mouse);
+	//graph_regist_mouse_handler(mouseEventHandler);	//Registar ações do rato
+	//graph_regist_timer_handler(timerEventHandler, TIME_TICK);	//Registar ações temporais
 	//times_register(timer,TIMER);
-	boardInit(&board);
-	draw_board(&board); //Função que tem o background_draw dentro!
+	boardInit(board);
+	draw_board(/*&board*/); //Função que tem o background_draw dentro!
 	graph_start(); //DEFINIR EVENTOS DE RATO AQUI!
 	
 }
