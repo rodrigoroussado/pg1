1acedc1cee99784ff3e8bb12bb75401c7f69b6c8
Rodrigo Roussado Viegas <121822846+rodrigoroussado@users.noreply.github.com>
2023-12-20 17:06:32 +0000

    Update cards.c

--- trab3/cards.c
+++ trab3/cards.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#define DEBUG 1
+
 
 void addPile(Board *board, Pile pile){
 	
@@ -67,16 +69,16 @@ void boardInit(Board *board){
 	board->points = 0;
 	board->tempo_jogo = 0;
 	
-	addPile(board, pileInit(PILETYPE_TOP, 0, X_DECK_STACK, Y_DECK_STACK)); //Deck pile
+	addPile(board, pileInit(PILETYPE_DOWN, 0, X_DECK_STACK, Y_DECK_STACK)); //Deck pile
  	criarBaralhoOrdenado(&board->piles[0]);
 	shuffle(&board->piles[0]);
 
-	addPile(board, pileInit(PILETYPE_DOWN, 1, X_DISCARD_STACK, Y_DISCARD_STACK)); //Discard pile
+	addPile(board, pileInit(PILETYPE_TOP, 1, X_DISCARD_STACK, Y_DISCARD_STACK)); //Discard pile
 	
-	addPile(board, pileInit(PILETYPE_DOWN, 0, X_FOUNDATION_STACK0, Y_FOUNDATION_STACK0));
-	addPile(board, pileInit(PILETYPE_DOWN, 0, X_FOUNDATION_STACK1, Y_FOUNDATION_STACK1));
-	addPile(board, pileInit(PILETYPE_DOWN, 0, X_FOUNDATION_STACK2, Y_FOUNDATION_STACK2));
-	addPile(board, pileInit(PILETYPE_DOWN, 0, X_FOUNDATION_STACK3, Y_FOUNDATION_STACK3));
+	addPile(board, pileInit(PILETYPE_TOP, 0, X_FOUNDATION_STACK0, Y_FOUNDATION_STACK0));
+	addPile(board, pileInit(PILETYPE_TOP, 0, X_FOUNDATION_STACK1, Y_FOUNDATION_STACK1));
+	addPile(board, pileInit(PILETYPE_TOP, 0, X_FOUNDATION_STACK2, Y_FOUNDATION_STACK2));
+	addPile(board, pileInit(PILETYPE_TOP, 0, X_FOUNDATION_STACK3, Y_FOUNDATION_STACK3));
 	
 	addPile(board, pileInit(PILETYPE_MIX, 1, X_CARD_STACK0, Y_CARD_STACK0));
 	addPile(board, pileInit(PILETYPE_MIX, 1, X_CARD_STACK1, Y_CARD_STACK1));
@@ -92,9 +94,11 @@ void boardInit(Board *board){
 
 void criarBaralhoOrdenado(Pile *p){
 	
-	for(int n = 0; n < p->nCards; n++){
+	p->nCards = 0;
+	
+	for(int n = 0; n < MAX_SUIT; n++){
 		
-		for(int j = 0; j < p->nCards; j++){
+		for(int j = 0; j < MAX_VALUE; j++){
 			
 			Card x;
 			x.suit = n;
@@ -108,19 +112,25 @@ void criarBaralhoOrdenado(Pile *p){
 }
 
 
-bool transferirCarta(Pile *pi, Pile *pf){ 
+bool transferirCarta(Board *board, int ind_pi, int ind_pf){ 
+	
+	#if DEBUG
+	printf("Transferir %d %d\n", ind_pi, ind_pf);
+	printf("Dimensões Iniciais %d %d\n", board->piles[ind_pi].nCards, board->piles[ind_pf].nCards);
+	#endif
 	
 	//Verificar se existem elementos na pilha inicial
-	if(pi->nCards == 0){return false;}
+	if(board->piles[ind_pi].nCards == 0){return false;}
 	
-	//Copiar elemento da posição dim-1 da pilha inicial para a posição dim da pilha final
-	pf->cards[pf->nCards] = pi->cards[(pi->nCards)];
+	//Copiar elemento da posição dim da pilha inicial para a posição dim da pilha final
+	board->piles[ind_pf].cards[board->piles[ind_pf].nCards].visible = true;
 	
-	//Atualizar nCards da pilha inicial (dim--)
-	pi->nCards--;
+	board->piles[ind_pf].cards[board->piles[ind_pf].nCards++] = board->piles[ind_pi].cards[board->piles[ind_pi].nCards--];
 	
-	//Atualizar nCards da pilha final (dim++)
-	pf->nCards++;
+	
+	#if DEBUG
+	printf("Dimensões Finais %d %d\n", board->piles[ind_pi].nCards, board->piles[ind_pf].nCards);
+	#endif
 	
 	return true;
 	
@@ -138,7 +148,7 @@ bool getCardName(Card c, char cardName[]) {
 	char suits[] = { 'd', 'h', 's', 'c' };
 	char values[] = { '2','3','4','5','6','7','8','9','t','q','j','k','a'};
 
-  	if ( ! c.visible ) sprintf(cardName, "%s%s", CARD_BACK, IMAGE_TYPE);
+  	if (  c.visible ) sprintf(cardName, "%s%s", CARD_BACK, IMAGE_TYPE);
   	else {
 	  if ( c.suit < 0 || c.suit >= MAX_SUIT || c.value < 0 || c.value >= MAX_VALUE ) return false;
 	  sprintf(cardName, "%c%c%s", suits[c.suit], values[c.value], IMAGE_TYPE);
